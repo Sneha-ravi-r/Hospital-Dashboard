dbb8d9aa5cb12ccff753f219e31cf2ca035aa3b3
Sneha-ravi-r <sneharavi2406@gmail.com>
2026-06-19 19:09:31 +0530

    Committing 30 items from workspace 070417f6-42c4-4759-adfe-4468c80892b4

---
+++ Trutesta_Sneha/Warehouse Training.Warehouse/dbo/Tables/trutesta_equipment equipment_checkout_type.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [dbo].[trutesta_equipment equipment_checkout_type] (
+
+	[id] varchar(8000) NULL, 
+	[tenant_id] varchar(8000) NULL, 
+	[name_text] varchar(8000) NULL, 
+	[business_code] varchar(8000) NULL, 
+	[description] varchar(8000) NULL, 
+	[status] int NULL, 
+	[checkin_checklist_id] varchar(8000) NULL, 
+	[placeholder_template_group_id] varchar(8000) NULL, 
+	[date_created] datetime2(6) NULL, 
+	[created_by_name] varchar(8000) NULL, 
+	[date_last_updated] datetime2(6) NULL, 
+	[lastupdated_by_name] varchar(8000) NULL, 
+	[document_upload_policy_id] varchar(8000) NULL
+);
