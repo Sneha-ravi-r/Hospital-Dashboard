c05bcd65aa4ad80ea238b8302e2585dd8f9e9aa9
Sneha-ravi-r <sneharavi2406@gmail.com>
2026-06-22 17:19:40 +0530

    Committing 36 items from workspace d1b4e9be-540b-4c44-8fd8-386437cbb29a

--- Trutesta_Sneha/WarehouseNew.Warehouse/dbo/Views/Omega.sql
+++
@@ -1,5 +0,0 @@
--- Auto Generated (Do not modify) C668233F8CAC1CF3FCA4BDC091D46D08735CB9A52071430764EA61C45575D727
-CREATE VIEW Omega AS 
-SELECT id, name_text,manufacturer_name
-FROM Equipment_Staging
-WHERE manufacturer_name = 'Omega';
